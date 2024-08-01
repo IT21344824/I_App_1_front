8362af75e5e41c622796ebb307942330fcf014e3
akidu <akiduekanayake@gmail.com>
2024-08-01 22:51:35 +0530

    cloudinary front push 1

--- src/pages/Uploads/Upload_pg.js
+++ src/pages/Uploads/Upload_pg.js
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 const Upload_pg = () => {
+    const [loading, setLoading] = useState(false);
+    const [url, setUrl] = useState("");
     const [file, setFile] = useState(null);
-    const [firstname, setFirstname] = useState("");
+    const [name, setName] = useState("");
+    const fileInputRef = useRef(null);
 
-    const handleUpload = async (e) => {
-        e.preventDefault();
+    const handleSubmit = async (event) => {
+        // Prevent default form submission
+        event.preventDefault(); 
+
+        if (!name) {
+            alert("Please enter a name");
+            return;
+        }
+
         if (!file) {
-            alert("Please select a file to upload");
+            alert("Please select an image");
             return;
         }
 
         const formData = new FormData();
-        formData.append("file", file);
-        formData.append("firstname", firstname);
 
+        // Append the name & file
+        formData.append('image', file);
+        formData.append('name', name); 
+
+        setLoading(true);
         try {
-            const response = await axios.post('http://localhost:5000/file/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
+            const res = await axios.post("http://localhost:5000/imgUpload/uploadImage", formData, {
+                headers: { 'Content-Type': 'multipart/form-data' }
             });
-            console.log(response.data);
-            alert("File uploaded successfully");
+            setUrl(res.data);
+            alert("Image uploaded successfully");
+
+            // Reset the inputs
+            setFile(null);
+            setName("");
+            // Reset the file input
+            fileInputRef.current.value = null; 
         } catch (error) {
-            console.error("There was an error uploading the file!", error);
-            alert("Error uploading file");
+            console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -34,30 +52,32 @@ const Upload_pg = () => {
         <div className='mt-5'>
             <div className="flex items-center justify-center p-12">
                 <div className="mx-auto w-full max-w-[550px] bg-white">
-                    <form className="py-6 px-9" onSubmit={handleUpload}>
-                        <div className="mb-6">
-                            <label className="mb-5 block text-xl font-semibold text-[#07074D]">
-                                Firstname
-                            </label>
-                            <input
-                                type="text"
-                                name="firstname"
-                                value={firstname}
-                                onChange={e => setFirstname(e.target.value)}
-                                className="w-full rounded-md border border-[#e0e0e0] py-2 px-4 text-base outline-none focus:border-[#6A64F1] focus:shadow-md"
-                            />
+                    <form className="py-6 px-9" onSubmit={handleSubmit}>
+                        <div className="mb-6 pt-4">
+                            <div className="mb-6">
+                                <label className="mb-5 block text-xl font-semibold text-[#07074D]">
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    className="w-full rounded-md border border-[#e0e0e0] py-2 px-4 text-base outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                />
+                            </div>
                         </div>
                         <div className="mb-6 pt-4">
                             <label className="mb-5 block text-xl font-semibold text-[#07074D]">
-                                Upload File
+                                Image Upload File
                             </label>
-
-
-                            <div class="space-y-8 font-[sans-serif]  max-w-md mx-auto">
-                                <input type="file"
-                                    onChange={e => setFile(e.target.files[0])}
-                                    className="w-full text-gray-500 font-medium text-sm bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded" />
-
+                            <div className="space-y-8 font-[sans-serif] max-w-md mx-auto">
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    ref={fileInputRef}
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                    className="w-full text-gray-500 font-medium text-sm bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded"
+                                />
                             </div>
                         </div>
                         <div>
@@ -65,7 +85,7 @@ const Upload_pg = () => {
                                 type="submit"
                                 className="mt-3 hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
                             >
-                                Send File
+                                {loading ? 'Uploading...' : 'Send File'}
                             </button>
                         </div>
                     </form>
